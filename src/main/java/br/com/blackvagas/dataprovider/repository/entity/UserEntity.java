4562c846d526b7dadf01006ad7b616c428bb4025
Rangel Soares <rangel.soares.souza@gmail.com>
2019-08-10 18:32:42 -0300

    configure database

--- src/main/java/br/com/blackvagas/dataprovider/repository/entity/UserEntity.java
+++ src/main/java/br/com/blackvagas/dataprovider/repository/entity/UserEntity.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "TUSER")
+@Table(name = "`TUSER`")
 public class UserEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
